14ae2db07c67db5d7d930aa048fb8eaeacac19a3
alexjwatts83 <alexjwatts83@gmail.com>
2021-07-17 08:21:58 +1000

    Added cors for localhost 4200 in the future, well I think, we'll see when the angular app starts

---
+++ API/Extensions/CorsAllowAllServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace API.Extensions
+{
+
+    public static class CorsAllowAllServiceExtensions
+    {
+        public static IServiceCollection AddCorsAllowAll(this IServiceCollection services)
+        {
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseCorsAllowAll(this IApplicationBuilder app)
+        {
+            app.UseCors("CorsPolicy");
+
+            return app;
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,8 @@ namespace API
             services.AddApplicationServices();
 
             services.AddSwaggerDocumentation();
+
+            services.AddCorsAllowAll();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +48,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCorsAllowAll();
 
             app.UseAuthorization();
 
