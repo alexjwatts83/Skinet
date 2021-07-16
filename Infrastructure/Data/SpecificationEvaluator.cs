ac4406a04acb29e3120e2fc983afd254af0bac34
alexjwatts83 <alexjwatts83@gmail.com>
2021-07-17 07:45:52 +1000

    added pagin details to specification

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -15,9 +15,22 @@ namespace Infrastructure.Data
                 query = query.Where(specification.Criteria);
             }
 
-            query = specification.Includes.Aggregate(query, (current, include) => current.Include(include));
+            if (specification.OrderBy != null)
+            {
+                query = query.OrderBy(specification.OrderBy);
+            }
 
-            return query;
+            if (specification.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(specification.OrderByDescending);
+            }
+
+            if (specification.IsPagingEnabled)
+            {
+                query = query.Skip(specification.Skip).Take(specification.Take);
+            }
+
+            return specification.Includes.Aggregate(query, (current, include) => current.Include(include));
         }
     }
 }
