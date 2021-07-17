e0336f7d597d837c647417f212fbc2b12e411380
Alex Watts <alexjwatts83@gmail.com>
2021-07-17 23:40:51 +1000

    fixed some loading issues where data is being displayed whilst the server is getting data

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -17,7 +17,7 @@ export class ErrorInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
-      delay(1000),
+      // delay(1000),
       catchError(error => {
         if (error) {
           if (error.status === 400) {
