174ad96cb850227a80685121eff71c1f4ba463cc
Alex Watts <alexjwatts83@gmail.com>
2021-07-17 21:35:12 +1000

    updated section header to dynamically set the heading based on the current bread crumb value

--- client/src/app/core/section-header/section-header.component.ts
+++ client/src/app/core/section-header/section-header.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-section-header',
   templateUrl: './section-header.component.html',
   styleUrls: ['./section-header.component.scss']
 })
-export class SectionHeaderComponent implements OnInit {
-
-  constructor() { }
+export class SectionHeaderComponent implements OnInit, OnDestroy {
+  title = 'Title';
+  breadcrumb$!: Observable<any[]>;
+  constructor(private bcService: BreadcrumbService) { }
 
   ngOnInit(): void {
+    this.breadcrumb$ = this.bcService.breadcrumbs$
   }
 
+  ngOnDestroy() : void {
+    
+  }
 }
